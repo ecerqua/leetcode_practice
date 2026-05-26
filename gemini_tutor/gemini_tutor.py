5ac4ac073c8062a9f0f8ea23a3f2508f8a531f5a
ecerqua <e.f.cerqua@gmail.com>
2026-05-25 20:00:48 -0400

    Create gemini tutor app and ui

--- gemini_tutor/gemini_tutor.py
+++ gemini_tutor/gemini_tutor.py
@@ -1,22 +1,25 @@
 from google import genai
+import asyncio
 
-def ai_tutor(prompt):
+async def ai_tutor(prompt):
     api_key = "AIzaSyC6EpTY47Zors4FfeyJ8XrLW5Ti2MgFwgk"
-    client = genai.Client(api_key=api_key)
+
+    model = "gemini-3.5-flash"
+    config = {"response_modalities": ["TEXT"]}
+
     system_prompt = "You are a helpful programming tutor answering questions" \
     "from your students. You should never give away the answer but should just" \
     "nudge them in the right direction when they don't understand. If they don't" \
     "have a specific question you should simply try explaing the problem" \
     "and the theory behind it more thoroughly."
 
-    response = client.models.generate_content(
-        model="gemini-3.5-flash",
-        contents=f"{system_prompt}\n\n---USER QUESTION---\n\n{prompt}")
-    
+    client = genai.Client(api_key=api_key)
+    response = await client.aio.models.generate_content(
+        model=model,
+        contents=f"{system_prompt}\n\n---STUDENT QUESTION---\n\n{prompt}",
+    )
     return response.text
-    
-
-
+        
 if __name__ == "__main__":
-    response = ai_tutor(prompt="testing")
-    print(response.text)
+    response = asyncio.run(ai_tutor(prompt="testing"))
+    print(response)
--- gemini_tutor/interface.py
+++ gemini_tutor/interface.py
@@ -1,27 +1,33 @@
 import customtkinter as ctk
 from customtkinter import CTkTextbox
 from gemini_tutor import ai_tutor
+import asyncio
+
+ctk.set_widget_scaling(1.0)
+ctk.set_widget_scaling(1.0)
+ctk.set_appearance_mode("dark")
 
 # Inititalize the main window
 root = ctk.CTk()
 root.title("Gemini Leetcode Tutor")
-root.geometry("740x1080")
+root.geometry("1920x1080")
 
 # Add a label to the prompt
-prompt_box_label = ctk.CTkLabel(root, text="Enter your question below:", font=("Arial", 18))
+prompt_box_label = ctk.CTkLabel(root, text="Enter your question below:", font=("Times New Roman", 18))
 prompt_box_label.pack(pady=5)
 
-prompt_box = CTkTextbox(root, width=550, height=300, font=("Arial", 16))
+prompt_box = CTkTextbox(root, width=740, height=300, font=("Times New Roman", 16))
 prompt_box.pack(pady=5, padx=5, fill=ctk.BOTH, expand=False)
 
 def get_response():
-    response = ai_tutor(prompt_box.get("1.0", ctk.END))
+    response = asyncio.run(ai_tutor(prompt_box.get("1.0", ctk.END)))
+    answer_box.delete("1.0", ctk.END)
     answer_box.insert("1.0", response)
 
-submit_button = ctk.CTkButton(root, text="Submit", command=get_response)
+submit_button = ctk.CTkButton(root, text="Submit", command=get_response, font=("Times New Roman", 18))
 submit_button.pack(pady=5)
 
-answer_box = CTkTextbox(root, width=550, height=300, font=("Arial", 16), )
+answer_box = CTkTextbox(root, width=740, height=600, font=("Times New Roman", 16), )
 answer_box.pack(pady=5, padx=5, fill=ctk.BOTH, expand=False)
 
 
